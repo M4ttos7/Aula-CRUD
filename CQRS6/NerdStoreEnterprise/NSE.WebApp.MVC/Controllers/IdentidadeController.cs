4d7e0829eaa11b7766854d1f23b6f68721630895
davi <davi.mattosc@gmail.com>
2023-04-03 16:24:08 -0300

    Html registro e login

---
+++ CQRS6/NerdStoreEnterprise/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //Api - Registro
+
+            if (false) return View(usuarioRegistro);
+
+            //Realizar login na Aplicação
+
+            return RedirectToAction("index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuariologin)
+        {
+            if (!ModelState.IsValid) return View(usuariologin);
+
+            //Api - Login
+
+            if (false) return View(usuariologin);
+
+            //Realizar login na Aplicação
+
+            return RedirectToAction("index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
